6387c61e525f3ce78b52acae4e38252ffd908352
wxz1998 <wxz18737676272@163.com>
2020-12-15 22:24:24 +0800

    main 20201215

--- src/main/java/com/wxz/edu/demo01/Employee.java
+++ src/main/java/com/wxz/edu/demo01/Employee.java
@@ -13,27 +13,22 @@ public class Employee {
     String designation;
     double salary;
 
-    // Employee 类的构造器
     public Employee(String name) {
         this.name = name;
     }
 
-    // 设置age的值
     public void empAge(int empAge) {
         age = empAge;
     }
 
-    /* 设置designation的值*/
     public void empDesignation(String empDesig) {
         designation = empDesig;
     }
 
-    /* 设置salary的值*/
     public void empSalary(double empSalary) {
         salary = empSalary;
     }
 
-    /* 打印信息 */
     public void printEmployee() {
         System.out.println("名字:" + name);
         System.out.println("年龄:" + age);
