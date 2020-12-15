fe23e2aa83083c15b8223bf16f7507f2e577e488
wxz1998 <wxz18737676272@163.com>
2020-12-15 21:45:48 +0800

    main 20201215

---
+++ src/main/java/com/wxz/edu/demo01/Puppy.java
@@ -0,0 +1,37 @@
+package com.wxz.edu.demo01;
+
+/**
+ * @TOOLS : Intellij IDEA
+ * @Author : Wu XianZhi
+ * @Date : 2020/12/14
+ * @Time : 22:36
+ * @Version : 1.0
+ */
+public class Puppy {
+    int puppyAge;
+
+    public Puppy(String name) {
+        // 这个构造器仅有一个参数：name
+        System.out.println("小狗的名字是 : " + name);
+    }
+
+    public void setAge(int age) {
+        puppyAge = age;
+    }
+
+    public int getAge() {
+        System.out.println("小狗的年龄为 : " + puppyAge);
+        return puppyAge;
+    }
+
+    public static void main(String[] args) {
+        /* 创建对象 */
+        Puppy myPuppy = new Puppy("tommy");
+        /* 通过方法来设定age */
+        myPuppy.setAge(2);
+        /* 调用另一个方法获取age */
+        myPuppy.getAge();
+        /*你也可以像下面这样访问成员变量 */
+        System.out.println("变量值 : " + myPuppy.puppyAge);
+    }
+}
