3772691fb867e2899952907c29a6c5a8550cc810
wxz1998 <wxz18737676272@163.com>
2020-12-16 22:12:36 +0800

    main 20201216

--- src/main/java/com/wxz/edu/demo04/Test.java
+++ src/main/java/com/wxz/edu/demo04/Test.java
@@ -46,5 +46,46 @@ public class Test {
         System.out.println("a < b = " + (a2 < b2));
         System.out.println("b >= a = " + (b2 >= a2));
         System.out.println("b <= a = " + (b2 <= a2));
+
+        // 位运算
+        int a3 = 60;
+        /* 60 = 0011 1100 */
+        int b3 = 13;
+        /* 13 = 0000 1101 */
+        int c3 = 0;
+        c3 = a3 & b3;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c3);
+
+        c3 = a3 | b3;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c3);
+
+        c3 = a3 ^ b3;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c3);
+
+        c3 = ~a3;
+        /*-61 = 1100 0011 */
+        System.out.println("~a = " + c3);
+
+        c3 = a3 << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c3);
+
+        c3 = a3 >> 2;
+        /* 15 = 1111 */
+        System.out.println("a >> 2  = " + c3);
+
+        c3 = a3 >>> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >>> 2 = " + c3);
+
+        // 逻辑运算
+        boolean a4 = true;
+        boolean b4 = false;
+        System.out.println("a && b = " + (a4 && b4));
+        System.out.println("a || b = " + (a4 || b4));
+        System.out.println("!(a && b) = " + !(a4 && b4));
     }
 }
