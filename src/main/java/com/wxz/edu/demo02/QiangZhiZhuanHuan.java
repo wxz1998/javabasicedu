4eb4cc987f25073b003345a57788765405a6daf2
wxz1998 <wxz18737676272@163.com>
2020-12-16 21:41:30 +0800

    main 20201215

--- src/main/java/com/wxz/edu/demo02/QiangZhiZhuanHuan.java
+++ src/main/java/com/wxz/edu/demo02/QiangZhiZhuanHuan.java
@@ -10,7 +10,8 @@ package com.wxz.edu.demo02;
 public class QiangZhiZhuanHuan {
     public static void main(String[] args) {
         int i1 = 123;
-        byte b = (byte)i1;//强制类型转换为byte
-        System.out.println("int强制类型转换为byte后的值等于"+b);
+        byte b = (byte) i1;
+        //强制类型转换为byte
+        System.out.println("int强制类型转换为byte后的值等于" + b);
     }
 }
