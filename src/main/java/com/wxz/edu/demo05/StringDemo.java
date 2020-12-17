faa64addbcc6570ac229ce8b50f27f4f86dd71bb
wxz1998 <wxz18737676272@163.com>
2020-12-17 11:14:26 +0800

    main 20201217

---
+++ src/main/java/com/wxz/edu/demo05/StringDemo.java
@@ -0,0 +1,16 @@
+package com.wxz.edu.demo05;
+
+/**
+ * @TOOLS : Intellij IDEA
+ * @Author : Wu XianZhi
+ * @Date : 2020/12/17
+ * @Time : 11:13
+ * @Version : 1.0
+ */
+public class StringDemo {
+    public static void main(String args[]) {
+        char[] helloArray = {'r', 'u', 'n', 'o', 'o', 'b'};
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
+    }
+}
