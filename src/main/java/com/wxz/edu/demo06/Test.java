649a1329a0f5e20a23bfd124e1f03cc22f85dbb0
wxz1998 <wxz18737676272@163.com>
2020-12-18 17:10:08 +0800

    main 20201218

---
+++ src/main/java/com/wxz/edu/demo06/Test.java
@@ -0,0 +1,18 @@
+package com.wxz.edu.demo06;
+
+/**
+ * @TOOLS : Intellij IDEA
+ * @Author : Wu XianZhi
+ * @Date : 2020/12/18
+ * @Time : 17:08
+ * @Version : 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer("菜鸟教程官网：");
+        stringBuffer.append("www");
+        stringBuffer.append(".runoob");
+        stringBuffer.append(".com");
+        System.out.println(stringBuffer);
+    }
+}
